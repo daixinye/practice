da4905a2462db976fcc497f21f14aa0302a51a18
Dxy <denight@qq.com>
2017-05-20 17:22:52 +0800

    实现了文本选中后添加至摘录本的基础功能

--- chrome-extension/excerptor/content.js
+++ chrome-extension/excerptor/content.js
@@ -1,14 +1,81 @@
 // 这里的脚本可以插入到 浏览器页面中
 
-document.getElementById('su').addEventListener('click', function(e) {
-	e.preventDefault();
-	sendMessage('test', function(res){
-		console.log(res);
-	})
-})
-
-function sendMessage(message,callback){
+function sendMessage(message, callback) {
 	chrome.extension.sendMessage(message, callback);
 }
 
+var menu = new Menu();
+var note = new Note();
+
+function Menu() {
+	this.lastSelected = "";
+	var body = document.querySelector('body');
+
+	body.onmouseup = function(e) {
+		menu.onmouseup(e);
+	}
+
+	body.onscroll = function(e) {
+		menu.hideMenu();
+	}
+
+	body.onclick = function(e){
+		var id = e.target.id;
+		switch(id){
+			case 'menu-add':
+				note.add(this.lastSelected);
+				break;
+		}
+	}.bind(this);
+}
+
+Menu.prototype.showMenu = function(x, y) {
+	var menu = this.getMenu();
+
+	menu.style.display = "block";
+	menu.style.top = y + "px";
+	menu.style.left = x + "px";
+}
+
+Menu.prototype.hideMenu = function() {
+	var menu = this.getMenu();
+	menu.style.display = "none";
+}
+
+Menu.prototype.createMenu = function() {
+	var menu = document.createElement('div');
+	menu.id = "menu";
+	menu.style.display = "block";
+	menu.style.position = "fixed";
+
+	menu.innerHTML = '<div id="menu-add">添加至摘录本</div>';
+
+	document.querySelector('body').appendChild(menu);
+
+	return menu;
+}
+
+Menu.prototype.getMenu = function() {
+	return document.querySelector('#menu') || this.createMenu();
+}
+
+Menu.prototype.onmouseup = function(e) {
+
+	var selectedText = window.getSelection().toString().trim();
+	selectedText ? this.showMenu(e.x, e.y) : this.hideMenu();
+	this.lastSelected = selectedText || this.lastSelected;
+}
+
+function Note() {
+	this.notes = [];
+}
+
+Note.prototype.createNote = function() {
+
+}
+
+Note.prototype.add = function(text) {
+	this.notes.push(text);
+	console.log('当前notes',this.notes);
+}
 
