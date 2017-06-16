ae8bcf401e66a7ba38bd46154392e7fb29ccf7f7
Dxy <denight@qq.com>
2017-06-16 11:38:25 +0800

    update

--- canvas/player/index.js
+++ canvas/player/index.js
@@ -1,26 +1,66 @@
-;void function(){
-    var $video = $('#J_video')
-    var $canvas = $('#J_canvas')
-    var $p = $('#J_p')
-    var canvas2D = $canvas[0].getContext('2d')
-
-    this.get
-
-    $video.on('play', function(){
-        // 隐藏video
-        $video.css('display','none')
-        setInterval(function(){
-            canvas2D.drawImage($video[0], 0,0,320,240)
-            console.log('drawing')
-        },1/60)
-    }).on('ended',function(){
-        console.log('end')
-    })
-}()
-
 class app {
     constructor(){
-        console.log('test')
+        this.$video = $('video')
+        this.timeoutFlag = 0
+
+        this.init()
+    }
+
+    init() {
+        this.bindEvents()
+        this.renderCanvas()
+    }
+
+    draw(c2d, timeout, v) {
+        setTimeout(() => {
+            if(this.timeoutFlag) return
+            c2d.drawImage(v, 0, 0, v.width, v.height)
+            this.draw(c2d, timeout, v)
+            console.log(1)
+        }, timeout);
+    }
+
+    startDraw (c2d, timeout, v){
+        this.timeoutFlag = 0
+        this.draw(c2d, timeout, v)
+    }
+    stopDraw(){
+        this.timeoutFlag = 1
+    }
+
+    bindEvents() {
+        this.$video.on('play', e => {
+            // todo: 隐藏$video 开始canvas绘制
+            
+            let $v = $(e.target)
+            let $c = $v.next('canvas')
+            let c2d = $c[0].getContext('2d')
+            this.startDraw(c2d, 1/30, e.target)
+            $v.hide()
+            $c.show()
+
+        })
+
+        this.$video.on('ended', e => {
+            // 显示$video
+            let $v = $(e.target)
+            let $c = $v.next('canvas')
+            this.stopDraw()
+            $c.hide()
+            $v.show()
+        })
+
+    }
+
+    renderCanvas(){
+        // 生成相应的canvas
+        this.$video.forEach((v, i) => {
+            let c = $('<canvas />')
+            c.attr("width", v.width)
+            c.attr("height", v.height)
+            c.hide()
+            this.$video.after(c)
+        })
     }
 }
 
