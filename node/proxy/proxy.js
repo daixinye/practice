62c4fcc15286fa7a9383ece57b289171d40abf88
Dxy <denight@qq.com>
2017-06-22 15:37:09 +0800

    正向代理

---
+++ node/proxy/proxy.js
@@ -0,0 +1,67 @@
+var http = require('http');
+var net = require('net');
+var url = require('url');
+
+function request(cReq, cRes) {
+    var u = url.parse(cReq.url);
+
+    if(hook(cReq, cRes)){
+        return
+    }
+
+    var options = {
+        hostname : u.hostname, 
+        port     : u.port || 80,
+        path     : u.path,       
+        method     : cReq.method,
+        headers     : cReq.headers
+    };
+
+    var pReq = http.request(options, function(pRes) {
+        cRes.writeHead(pRes.statusCode, pRes.headers);
+        pRes.pipe(cRes);
+    }).on('error', function(e) {
+        cRes.end();
+    });
+
+    cReq.pipe(pReq);
+}
+
+function connect(cReq, cSock) {
+    var u = url.parse('http://' + cReq.url);
+
+    var pSock = net.connect(u.port, u.hostname, function() {
+        cSock.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+        pSock.pipe(cSock);
+    }).on('error', function(e) {
+        cSock.end();
+    });
+
+    cSock.pipe(pSock);
+}
+
+http.createServer()
+    .on('request', request)
+    .on('connect', connect)
+    .listen(8888, '0.0.0.0');
+
+function hook(cReq, cRes){
+    // 如果满足某种条件
+    var u = url.parse(cReq.url);
+    console.log(u)
+    if(u.hostname == "api.yuerbao.com"){
+        sendJSON(cRes, {
+            mock: true,
+            message: 'mock success'
+        })
+        return true
+    }
+    return false
+}
+
+function sendJSON(cRes, data){
+    cRes.writeHead(200, {
+        'Content-Type': 'text/json'
+    })
+    cRes.end(JSON.stringify(data))
+}
