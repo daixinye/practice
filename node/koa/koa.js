b90f861387d5701a53b57d475f09e3857196a371
Dxy <denight@qq.com>
2017-07-03 16:54:02 +0800

    微信浏览器判断

--- node/koa/koa.js
+++ node/koa/koa.js
@@ -1,14 +1,13 @@
 let koa = require('koa')
 let app = new koa();
+let fs = require('fs')
 
 app.listen(3000)
 
 app.use(async (ctx, next) => {
-    console.log(ctx.headers)
     await next()
-
 })
 
 app.use(async (ctx, next) => {
-    ctx.body = 'hello next'
+    ctx.body = fs.readFileSync('./index.html','utf-8')
 })
