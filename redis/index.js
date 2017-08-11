038ad7464fb3f297abc155ed5a675b4113d8945e
Dxy <denight@qq.com>
2017-08-12 00:49:38 +0800

    [update] redis

--- redis/index.js
+++ redis/index.js
@@ -5,8 +5,16 @@ client.on('error', function(err){
     console.log('Error', err)
 })
 
-client.set('color', 'red', redis.print)
-client.get('color', function(err, value){
+client.set('test:key1', 'value1', redis.print)
+client.mset(['test:key2','value2','test:key3','value3'], redis.print)
+
+client.get('test:key1', function(err, value){
     if(err) throw err
     console.log('Got:', value)
 })
+
+client.keys('test:*', function(err, value){
+    if(err) throw err
+    console.log('Keys:', value)
+})
+
